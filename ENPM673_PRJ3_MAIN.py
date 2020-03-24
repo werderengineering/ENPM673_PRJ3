740f45cf328591834c6ff5d9a2d2f7346f1a6a14
Werder <werdertimothy@gmail.com>
2020-03-24 13:25:26 -0400

    Imports and video

--- ENPM673_PRJ3_MAIN.py
+++ ENPM673_PRJ3_MAIN.py
@@ -17,9 +17,21 @@ def main(prgRun):
     problem = 1
     # problem = int(input('Which part would you like to run? \nEnter 1 for ngihtime image . \nEnter 2 for the firt part of the lane finder. \nEnter 3 for the second part of the lane finder (Challenge video): '))
 
-    #Correct image
-    if problem ==1:
-        print('Being Problem')
+    video = cv2.VideoCapture('detectbuoy.avi')
+
+    # Read until video is completed
+    while (video.isOpened()):
+        # Capture frame-by-frame
+        ret, frame = video.read()
+        if ret == True:
+            frame = imutils.resize(frame, width=320, height=180)
+            frame.shape
+            ogframe = frame
+            clnframe = frame
+            resetframe = frame
+            cv2.imshow('Original Frame', frame)
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                break
 
 
     prgRun=False
